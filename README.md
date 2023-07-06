3da6f6e79adc97fbd4e0303683a01973b74a2d26
JorgeJDev <jjuansanda@gmail.com>
2023-07-06 18:59:07 +0200

    primeros endpoints de prueba

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Primera API CRUD de Python con Flask y PostgreSQL
 
+### Postman: https://documenter.getpostman.com/view/26092541/2s93zFXzVg
--- app.py
+++ app.py
@@ -13,15 +13,26 @@ def get_connection() :
     conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
     return conn
 
-@app.get('/')
-def home():
-    conn = get_connection()
-    cur = conn.cursor()
-    
-    cur.execute("SELECT 1 + 1")
-    result = cur.fetchone()
-    print(result)
-    return 'Hello World!'
+@app.get('/api/v1/users')
+def get_users():
+    return 'getting users'
+
+@app.get('/api/v1/users/1')
+def get_user():
+    return 'getting user'
+
+@app.post('/api/v1/users')
+def create_users():
+    return 'creating users'
+
+@app.put('/api/v1/users/1')
+def update_users():
+    return 'updating users'
+
+@app.delete('/api/v1/users/1')
+def delete_users():
+    return 'deleting users'
+
 
 if __name__ == '__main__' :
     app.run(debug=True)
