31ae6ae97ddf6f898308d647b032777092f51014
JorgeJDev <jjuansanda@gmail.com>
2023-07-06 18:46:05 +0200

    conexion a psql

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Primera API CRUD de Python con Flask y PostgreSQL
 
-- Crear máquina virtual 
-
--- pip install virtualenv
-
--- python -m virtualenv venv
--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from psycopg2 import connect
 
 app = Flask(__name__)
+
+host = 'localhost'
+port = 5432
+dbname = 'flask1'
+user = 'postgres'
+password = 'admin'
+
+def get_connection() :
+    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    return conn
+
+@app.get('/')
+def home():
+    conn = get_connection()
+    cur = conn.cursor()
+    
+    cur.execute("SELECT 1 + 1")
+    result = cur.fetchone()
+    print(result)
+    return 'Hello World!'
 
 if __name__ == '__main__' :
     app.run(debug=True)
